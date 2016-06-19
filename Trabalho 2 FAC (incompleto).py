7e121426b755ee518fe182c2320d0800914888cd
Rafael Lannes <rfarias306@hotmail.com>
2016-06-19 16:39:25 -0300

    Update Trabalho 2 FAC (incompleto).py

--- Trabalho 2 FAC (incompleto).py
+++ Trabalho 2 FAC (incompleto).py
@@ -19,21 +19,8 @@ def dez_para_binario(numero):#Função para converter decimal para binario (só 
     inverso.append(num)
     binario = inverter_vetor(inverso)
     return binario
-
-def dec_to_bin(x):
-    return int(bin(x)[2:])#Função que retorna o valor binário de um numero decimal.
-
-def expoentebin(entrada):#Função que retorna o expoente em binário já com o excesso de 127.
-    nbin=entrada.split(',')
-    r=(len(nbin[0])-1)+127
-    return dec_to_bin(r)
-
-def virgula(entrada): #Função move a virgula para a primeira casa e retorna a string.
-    r=entrada.split(',')
-    r=r[0]+r[1]
-    r=r[0]+','+r[1:]
-    return r
-
+    
+    
 def casos_especiais(entrada): #Imprime no formato IEEE754 e Retorna True se for um caso especial.
     if entrada=='0' or entrada=='+0':
         print('SINAL: 0 EXPOENTE: 00000000 MANTISSA: 00000000000000000000000')
